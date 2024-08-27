b6e68c2e0ad5f1856972b845a42286538b5aa0f2
Adityasputra <aditsputra.cv@gmail.com>
2024-08-27 17:09:20 +0700

    Feat: create getAll users model and get all by id model

---
+++ app/src/db/models/user.ts
@@ -0,0 +1,42 @@
+import { ObjectId } from "mongodb";
+import { getMongoClientInstance } from "../config";
+
+const DATABASE_NAME = "games-store-db";
+const COLLECTION_USER = "users";
+
+export interface UserModel {
+  _id: ObjectId;
+  username: string;
+  email: string;
+  password: string;
+}
+
+export type UserModelInput = Omit<UserModel, "_id">;
+export const getDB = async () => {
+  const client = await getMongoClientInstance();
+  const db = client.db(DATABASE_NAME);
+
+  return db;
+};
+
+export const getAllUsers = async () => {
+  const db = await getDB();
+  const users = (await db
+    .collection(COLLECTION_USER)
+    .find()
+    .project({ password: 0 })
+    .toArray()) as UserModel[];
+
+  return users;
+};
+
+export const getUserById = async (id: string) => {
+  const db = await getDB();
+  const objectId = new ObjectId(id);
+
+  const user = (await db
+    .collection(COLLECTION_USER)
+    .findOne({ _id: objectId }, { projection: { password: 0 } })) as UserModel;
+
+  return user;
+};
