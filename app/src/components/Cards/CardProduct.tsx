d3bcb32a0546acc02867d98349a82952c21c5ac6
Adityasputra <aditsputra.cv@gmail.com>
2024-09-01 22:57:15 +0700

    Fix: memperbaiki tampilan sidebar dan card products

--- app/src/components/Cards/CardProduct.tsx
+++ app/src/components/Cards/CardProduct.tsx
@@ -15,43 +15,47 @@ export default function CardProduct({
   wishlistStatus,
 }: ProductCardProps) {
   return (
-    <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
+    <div className="bg-gray-800 rounded-lg shadow-md overflow-hidden flex flex-col">
       <img
         className="w-full h-48 object-cover"
         src={product.thumbnail}
         alt={product.name}
       />
       <div className="flex flex-col p-4 flex-1">
-        <h2 className="text-lg font-bold mb-2">{product.name}</h2>
-        <div className="text-sm text-gray-600 mb-2">
-          {product.tags?.join(", ")}
-        </div>
-        <p className="text-xl font-semibold text-gray-800 mb-4">
+        <h2 className="text-lg font-bold mb-2 text-gray-100">{product.name}</h2>
+        <p className="text-sm text-gray-100 mb-4">{product.excerpt}</p>
+        <p className="text-xl font-semibold text-gray-100 mb-4">
           ${product.price?.toFixed(2)}
         </p>
-        <p className="text-sm text-gray-600 mb-4">{product.excerpt}</p>
+        <div className="text-sm text-gray-100 mb-2">
+          {product.tags?.map((c) => (
+            <span key={c} className="bg-gray-700 p-2 rounded mx-1">
+              {c}
+            </span>
+          ))}
+        </div>
         <div className="flex justify-between items-center mt-auto">
           <button
             onClick={() => handleAddToWishlist("product._id")}
-            className="text-blue-500 hover:text-blue-700"
+            className="text-blue-100 hover:text-gray-600"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth="1.5"
+              stroke-width="1.5"
               stroke="currentColor"
-              className="w-6 h-6"
+              className="size-6"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 8.75l4.5 4.5 6-6M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
               />
             </svg>
           </button>
           <Link href={`/products/${product._id}`}>
-            <button className="px-4 py-2 bg-blue-500 text-white text-sm font-medium rounded-md hover:bg-blue-600">
+            <button className="px-4 py-2 bg-gray-900 text-white text-sm font-medium rounded-md hover:bg-gray-700">
               View Details
             </button>
           </Link>
--- app/src/components/Sidebar.tsx
+++ app/src/components/Sidebar.tsx
@@ -19,8 +19,8 @@ export default function Sidebar({
   const [isDropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <aside className="flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
-      <Link href="/" className="text-lg font-bold text-center">
+    <aside className="flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto bg-gray-800">
+      <Link href="/" className="text-lg font-bold text-center text-gray-100">
         LevelUp Games
       </Link>
       <div className="relative mt-6">
@@ -43,7 +43,7 @@ export default function Sidebar({
           type="text"
           value={searchTerm}
           onChange={handleSearchChange}
-          className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
+          className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-white focus:ring-opacity-40 focus:outline-none focus:ring"
           placeholder="Search"
         />
       </div>
@@ -51,27 +51,28 @@ export default function Sidebar({
         <nav>
           <Link
             href="/"
-            className="flex items-center px-4 py-2 text-gray-700 bg-gray-100 rounded-md dark:bg-gray-800 dark:text-gray-200"
+            className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700 rounded-md"
           >
             <svg
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-              fill="none"
               xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
             >
               <path
-                d="M19 11H5M19 11C20.1046 11 21 11.8954 21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V13C3 11.8954 3.89543 11 5 11M19 11V9C19 7.89543 18.1046 7 17 7M5 11V9C5 7.89543 5.89543 7 7 7M7 7V5C7 3.89543 7.89543 3 9 3H15C16.1046 3 17 3.89543 17 5V7M7 7H17"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122"
               />
             </svg>
+
             <span className="mx-4 font-medium">Dashboard</span>
           </Link>
           <Link
             href="/wishlist"
-            className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+            className="flex items-center px-4 py-2 mt-5 text-gray-100 transition-colors duration-300 transform rounded-md hover:bg-gray-700 hover:text-gray-100"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -90,14 +91,14 @@ export default function Sidebar({
             <span className="mx-4 font-medium">Wishlist</span>
           </Link>
 
-          <hr className="my-6 border-gray-200 dark:border-gray-600" />
+          <hr className="my-6 border-gray-600" />
 
           <div className="relative">
             <button
               onClick={() => setDropdownOpen(!isDropdownOpen)}
-              className={`flex items-center justify-between w-full px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 ${
-                isDropdownOpen ? "bg-gray-200 dark:bg-gray-700" : ""
-              } hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700`}
+              className={`flex items-center justify-between w-full px-4 py-2 mt-5 text-gray-100 transition-colors duration-300 transform rounded-md dark:text-gray-400 ${
+                isDropdownOpen ? "bg-gray-700 dark:bg-gray-700" : ""
+              } hover:bg-gray-700 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-100`}
             >
               Platforms
               <svg
@@ -116,7 +117,7 @@ export default function Sidebar({
             </button>
 
             {isDropdownOpen && (
-              <div className="absolute z-10 w-full mt-2 bg-white rounded-md shadow-lg dark:bg-gray-700">
+              <div className="absolute z-10 w-full mt-2 bg-gray-700 rounded-md shadow-lg dark:bg-gray-700">
                 {categories.map((category) => (
                   <button
                     key={category}
@@ -126,8 +127,8 @@ export default function Sidebar({
                     }}
                     className={`block w-full px-4 py-2 text-left ${
                       selectedCategory === category
-                        ? "bg-gray-200 dark:bg-gray-600 text-gray-800 dark:text-white"
-                        : "text-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 hover:text-gray-900 dark:hover:text-white"
+                        ? "bg-gray-700 dark:bg-gray-600 text-gray-100 dark:text-white"
+                        : "text-gray-100 dark:text-gray-400 hover:bg-gray-700 dark:hover:bg-gray-800 hover:text-gray-500 dark:hover:text-white"
                     }`}
                   >
                     {category}
