4965bc7f9bd005a5f5994df620c6b42a6add943f
Adityasputra <aditsputra.cv@gmail.com>
2024-09-01 23:52:16 +0700

    Fix: fix style  wishlist pages

--- app/src/app/(auth)/wishlist/page.tsx
+++ app/src/app/(auth)/wishlist/page.tsx
@@ -3,62 +3,27 @@
 import { useEffect, useState } from "react";
 import { BASE_URL } from "@/constants";
 import Link from "next/link";
+import { Wishlist } from "@/db/models/wishlist";
 
-interface Product {
-  _id: string;
-  name: string;
-  slug: string;
-  description: string;
-  excerpt: string;
-  price: number;
-  tags: string[];
-  thumbnail: string;
-  images: string[];
-  createdAt: string;
-  updatedAt: string;
-}
-
-interface WishlistItem {
-  _id: string;
-  userId: string;
-  productId: string;
-  createdAt: string;
-  updatedAt: string;
-  product: Product;
-  user: {
-    _id: string;
-    name: string;
-    username: string;
-    email: string;
-  };
-}
-
-const Wishlist: React.FC = () => {
-  const [wishlistItems, setWishlistItems] = useState<WishlistItem[]>([]);
+export default function WishlistPages() {
+  const [wishlistItems, setWishlistItems] = useState<Wishlist[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchWishlist = async () => {
       try {
-        const userId = localStorage.getItem("userId");
+        const res = await fetch(`${BASE_URL}/api/wishlist`);
 
-        if (!userId) {
-          throw new Error("User is not logged in.");
-        }
-
-        const response = await fetch(
-          `${BASE_URL}/api/wishlist?userId=${userId}`
-        );
-
-        if (!response.ok) {
+        if (!res.ok) {
           throw new Error("Failed to fetch wishlist.");
         }
 
-        const data = await response.json();
+        const data = await res.json();
         setWishlistItems(data || []);
       } catch (error) {
-        console.log(error);
+        console.error("Error fetching wishlist:", error);
+        setError("Failed to fetch wishlist.");
       } finally {
         setLoading(false);
       }
@@ -68,7 +33,7 @@ const Wishlist: React.FC = () => {
   }, []);
 
   if (loading) {
-    return <p>Loading...</p>;
+    return <p className="text-gray-600">Loading...</p>;
   }
 
   if (error) {
@@ -77,19 +42,20 @@ const Wishlist: React.FC = () => {
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold">Your Wishlist</h1>
-      <div className="flex flex-wrap -mx-4 mt-4">
+      <h1 className="text-2xl font-bold mb-6">Your Wishlist</h1>
+      <div className="flex flex-wrap -mx-4">
         {wishlistItems.length > 0 ? (
           wishlistItems.map((item) => (
+            console.log(item),
             <div
               key={item.product._id}
-              className="p-4 bg-white rounded-lg shadow-md border border-gray-200 flex flex-col justify-between mx-4 mb-4 flex-1 min-w-[300px]"
+              className="p-4 bg-white rounded-lg shadow-md border border-gray-200 flex flex-col justify-between mx-4 mb-4 flex-1 min-w-[300px] transition-transform transform hover:scale-105"
             >
               {item.product.thumbnail && (
                 <img
                   src={item.product.thumbnail}
                   alt={item.product.name}
-                  className="w-full h-48 object-cover mb-4 rounded-t-lg"
+                  className="w-full h-48 object-cover mb-4 rounded-lg"
                 />
               )}
               <div className="flex flex-col flex-1">
@@ -99,7 +65,9 @@ const Wishlist: React.FC = () => {
                 <p className="text-gray-600 mb-2 flex-1">
                   {item.product.description}
                 </p>
-                <p className="font-bold text-lg mb-4">${item.product.price}</p>
+                <p className="font-bold text-lg mb-4">
+                  ${item.product.price.toFixed(2)}
+                </p>
                 <div className="flex flex-wrap gap-2 mb-4">
                   {item.product.tags.map((tag) => (
                     <span
@@ -113,7 +81,7 @@ const Wishlist: React.FC = () => {
                 <div className="flex justify-between items-center mt-4">
                   <Link
                     href={`/products/${item.product._id}`}
-                    className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                    className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300"
                   >
                     View Details
                   </Link>
@@ -122,11 +90,9 @@ const Wishlist: React.FC = () => {
             </div>
           ))
         ) : (
-          <p>No products in your wishlist.</p>
+          <p className="text-gray-600">No products in your wishlist.</p>
         )}
       </div>
     </div>
   );
-};
-
-export default Wishlist;
+}
--- app/src/components/Sidebar.tsx
+++ app/src/components/Sidebar.tsx
@@ -57,7 +57,7 @@ export default function Sidebar({
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="size-6"
             >
@@ -78,7 +78,7 @@ export default function Sidebar({
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="size-6"
             >
